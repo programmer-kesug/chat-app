6001d5bbe8c59c82075ad5da2c44f808d464208e
dev.kesug <dev.kesug@gmail.com>
2026-03-06 18:29:24 +0700

    no duplicate names + emoji + sticker

--- server.js
+++ server.js
@@ -12,24 +12,25 @@ const users = {};
 
 io.on('connection', (socket) => {
   socket.on('join', (name) => {
+    const taken = Object.values(users).some(n => n === name);
+    if (taken) { socket.emit('nameTaken'); return; }
     socket.username = name;
     users[socket.id] = name;
+    socket.emit('joined');
     io.emit('msg', { user: 'System', text: name + ' เข้าร่วมแล้ว' });
-    io.emit('users', Object.values(users));
+    io.emit('users', Object.entries(users).map(([id,name]) => ({id,name})));
   });
   socket.on('msg', (data) => {
-    io.emit('msg', data);
+    io.emit('msg', { ...data, sid: socket.id });
   });
   socket.on('disconnect', () => {
     if (socket.username) {
       delete users[socket.id];
       io.emit('msg', { user: 'System', text: socket.username + ' ออกไปแล้ว' });
-      io.emit('users', Object.values(users));
+      io.emit('users', Object.entries(users).map(([id,name]) => ({id,name})));
     }
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
-  console.log('Server running on port ' + PORT);
-});
+server.listen(PORT, '0.0.0.0', () => console.log('Server running on port ' + PORT));
